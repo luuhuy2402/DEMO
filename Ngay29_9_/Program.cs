c156c53d8af081234d582594636436da9301d2d1
luuhuy2402 <xeoxeo1895@gmail.com>
2023-10-21 09:00:59 +0700

    fix 29_9

--- Ngay29_9_/Program.cs
+++ Ngay29_9_/Program.cs
@@ -14,6 +14,7 @@ namespace Ngay29_9_
         [STAThread]
         static void Main()
         {
+            // toi da sua
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
